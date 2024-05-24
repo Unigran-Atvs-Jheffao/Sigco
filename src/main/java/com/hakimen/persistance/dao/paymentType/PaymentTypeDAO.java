bbc64ebc5659aabdcb2796c23f2681511bd01b2a
GeneralMoraes <gustavosouzamoraes70@gmail.com>
2024-05-24 18:59:32 -0300

    Novos DTO Alguns Controles Terminado de fazer os auxiliares no DAO

---
+++ src/main/java/com/hakimen/persistance/dao/paymentType/PaymentTypeDAO.java
@@ -0,0 +1,7 @@
+package com.hakimen.persistance.dao.paymentType;
+
+import com.hakimen.model.auxiliar.PaymentType;
+import com.hakimen.persistance.dao.DAO;
+
+public interface PaymentTypeDAO extends DAO<Integer, PaymentType> {
+}
